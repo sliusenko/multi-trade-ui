20f50dcda6730b8111b39f4b8b8c114da7d9a2c5
sliusenko <a.slusenko@gmail.com>
2025-08-02 17:38:53 +0300

    create ui

--- app/services/db.py
+++ app/services/db.py
@@ -3,7 +3,7 @@ from databases import Database
 
 DB_USER = os.getenv("DB_USER", "bot")
 DB_PASS = os.getenv("DB_PASS", "00151763Db3c")
-DB_HOST = os.getenv("DB_HOST", "host.docker.internal")
+DB_HOST = os.getenv("DB_HOST", "172.19.0.1")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "multi_trade_bot")
 
