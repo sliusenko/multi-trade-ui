59515d75f97c7af0a566235592ef8e9a64ea91fe
sliusenko <a.slusenko@gmail.com>
2025-08-02 17:41:32 +0300

    create ui

--- app/services/db.py
+++ app/services/db.py
@@ -5,7 +5,7 @@ DB_USER = os.getenv("DB_USER", "bot")
 DB_PASS = os.getenv("DB_PASS", "00151763Db3c")
 DB_HOST = os.getenv("DB_HOST", "172.19.0.1")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "multi_trade_bot")
+DB_NAME = os.getenv("DB_NAME", "tradebot")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
