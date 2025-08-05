9155dc05c9301a0e45dccabbc94f85e9a3c62691
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-08-05 14:18:40 +0300

    Create dependencies.py

---
+++ app/dependencies.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+# Простий варіант з токеном в Authorization: Bearer <token>
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    # Тут можна робити перевірку токена, поки повернемо фіктивного користувача
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return 1  # Наприклад, user_id = 1
