3f3602809b9fcc8b54188448d8cd4a5f13a230e9
sliusenko <a.slusenko@gmail.com>
2025-08-05 20:11:10 +0300

    create ui

--- app/services/db.py
+++ app/services/db.py
@@ -1,5 +1,7 @@
 import os
 from databases import Database
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 DB_USER = os.getenv("DB_USER", "bot")
 DB_PASS = os.getenv("DB_PASS", "00151763Db3c")
@@ -11,3 +13,5 @@ DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{D
 
 # Async Database instance
 database = Database(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
