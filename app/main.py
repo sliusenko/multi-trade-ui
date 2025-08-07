ead9c42cf7a9e6e60055deee4ff0ccf663c506ae
sliusenko <a.slusenko@gmail.com>
2025-08-07 19:02:47 +0300

    create ui

--- app/main.py
+++ app/main.py
@@ -32,10 +32,6 @@ rules = [
     {"id": 2, "action": "Sell", "condition_type": "RSI>70", "enabled": False},
 ]
 
-print("✅ Routers initialized:")
-for route in app.routes:
-    print(f"{route.path} → {route.methods}")
-
 # === Події старта та завершення ===
 @app.on_event("startup")
 async def startup():
