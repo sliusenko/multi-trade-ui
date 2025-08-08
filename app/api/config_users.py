3b024c2b21f0a4da771ec8226165e6c147d7fb8e
sliusenko <a.slusenko@gmail.com>
2025-08-08 12:38:58 +0300

    create ui

--- app/api/config_users.py
+++ app/api/config_users.py
@@ -4,15 +4,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.auth.dependencies import get_db
 from app import models
 from app.schemas import user_config
+from app.services.db import database
+from typing import Optional, List
+from app.dependencies import get_current_user
+from app.models import users
+
 
 router = APIRouter(prefix="/api/config/users", tags=["User Config"])
 
 
-@router.get("/", response_model=list[user_config.UserOut])
-async def get_users(db: AsyncSession = Depends(get_db)):
+@router.get("/", response_model=List[user_config.UserOut])
+async def get_users(current_user_id: int = Depends(get_current_user)):
     """Отримати всіх користувачів"""
-    result = await db.execute(select(models.User))
-    return result.scalars().all()
+    query = select(users)  # SQLAlchemy Core
+    rows = await database.fetch_all(query)
+    return [user_config.UserOut(**dict(row)) for row in rows]
 
 
 @router.post("/", response_model=user_config.UserOut)
