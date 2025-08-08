75985bd207e2883fcf22a6ae230ba148b75d20f9
sliusenko <a.slusenko@gmail.com>
2025-08-08 10:59:35 +0300

    create ui

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert, select
-
+from datetime import timedelta
 from app.models import users
 from app.auth.jwt_handler import create_access_token, get_password_hash, verify_password
-from app.auth.dependencies import get_db
+from app.auth.dependencies import get_db, get_current_user
 from app.auth.schemas import RegisterRequest, LoginRequest
 
 router = APIRouter(tags=["Auth"])
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 @router.post("/register")
 async def register(data: RegisterRequest, db: AsyncSession = Depends(get_db)):
@@ -25,8 +26,18 @@ async def register(data: RegisterRequest, db: AsyncSession = Depends(get_db)):
 async def login(data: LoginRequest, db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(users).where(users.c.username == data.username))
     db_user = result.first()
+
     if not db_user or not verify_password(data.password, db_user._mapping["password_hash"]):
         raise HTTPException(status_code=401, detail="Invalid username or password")
 
-    token = create_access_token({"sub": str(db_user._mapping["user_id"])})
-    return {"access_token": token, "token_type": "bearer"}
+    expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    token = create_access_token(
+        data={"sub": str(db_user._mapping["user_id"])},
+        expires_delta=expires_delta
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "expires_in": int(expires_delta.total_seconds())  # важливо
+    }
