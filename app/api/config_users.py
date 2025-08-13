25ad61183c81e0480dc82f21fcfe70111d0fbf4a
sliusenko <a.slusenko@gmail.com>
2025-08-13 18:52:26 +0300

    create ui

--- app/api/config_users.py
+++ app/api/config_users.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.auth.dependencies import get_db
 from app import models
 from app.schemas import user_config
 from app.services.db import database
 from typing import Optional, List
-from app.dependencies import get_current_user
+from app.dependencies import get_current_user, get_db
 from app.auth.jwt_handler import hash_password
 from app.models import users
 
