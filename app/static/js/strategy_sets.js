b3c45c7f55601c4fad3912069a79eaf42cf9d663
sliusenko <a.slusenko@gmail.com>
2025-09-12 21:38:52 +0300

    Revert "added adx"
    This reverts commit e77207e065da5fbeb67a175e4a7dfed1ce1533e5.

--- app/static/js/strategy_sets.js
+++ app/static/js/strategy_sets.js
@@ -129,7 +129,7 @@ async function addSet() {
 
 async function loadSets() {
   const tbody = document.getElementById('setsTable');
-  if (tbody) tbody.innerHTML = `<tr><td colspan="7">Loading…</td></tr>`;
+  if (tbody) tbody.innerHTML = `<tr><td colspan="8">Loading…</td></tr>`;
 
   // беремо ТІЛЬКИ exchange/pair (user_id все одно приходить з токена на бекенді)
   const { exchange, pair, user_id } = getActiveFilters();
@@ -137,7 +137,7 @@ async function loadSets() {
 
   const res = await apiFetch(url);
   if (!res.ok) {
-    if (tbody) tbody.innerHTML = `<tr><td colspan="7">Error ${res.status}</td></tr>`;
+    if (tbody) tbody.innerHTML = `<tr><td colspan="8">Error ${res.status}</td></tr>`;
     return;
   }
   const sets = await res.json();
